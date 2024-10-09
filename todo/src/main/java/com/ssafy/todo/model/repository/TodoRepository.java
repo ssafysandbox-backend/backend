c095719c97a6f4b3750fd210d605133909db8e0c
JIYOUNG KIM <kandallee38@gmail.com>
2024-10-10 00:35:45 +0900

    feat: Add feature find by id
    Add feature find by id using in update
    Return Optional object

--- todo/src/main/java/com/ssafy/todo/model/repository/TodoRepository.java
+++ todo/src/main/java/com/ssafy/todo/model/repository/TodoRepository.java
@@ -5,6 +5,7 @@ import com.ssafy.todo.model.domain.TodoVO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo,Long> {
 
@@ -15,4 +16,7 @@ public interface TodoRepository extends JpaRepository<Todo,Long> {
 
     @Override
     void deleteById(Long todoId);
+
+    @Override
+    Optional<Todo> findById(Long todoId);
 }
