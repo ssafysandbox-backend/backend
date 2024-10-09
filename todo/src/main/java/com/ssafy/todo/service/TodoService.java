c05406b3a15a4d69f2c5b565c6939098f8c8312e
JIYOUNG KIM <kandallee38@gmail.com>
2024-10-09 23:03:08 +0900

    feat: Add findAll method
    Add findAll method using jpa repository

---
+++ todo/src/main/java/com/ssafy/todo/service/TodoService.java
@@ -0,0 +1,20 @@
+package com.ssafy.todo.service;
+
+import com.ssafy.todo.model.domain.Todo;
+import com.ssafy.todo.model.domain.TodoVO;
+import com.ssafy.todo.model.repository.TodoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class TodoService {
+
+    private final TodoRepository todoRepository;
+
+    public List<TodoVO> findAll() {
+        return todoRepository.findAllProjectedByOrderByIdDesc();
+    }
+}
