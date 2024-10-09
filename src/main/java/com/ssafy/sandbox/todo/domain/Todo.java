056a47fa485ce10d960c43907f7e75d825d935d9
leedongkyu0407 <use02012@gmail.com>
2024-10-09 20:11:22 +0900

    feat:Todo Entity Class 구현 - Id(Primary key), content(내용), isDone(완수 여부) 필드 정의 - MySQL DB 연동 - Entity와 매핑되도록 'todos' 테이블 생성

---
+++ src/main/java/com/ssafy/sandbox/todo/domain/Todo.java
@@ -0,0 +1,18 @@
+package com.ssafy.sandbox.todo.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name="todos")
+@Getter @Setter @ToString
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String content;
+    private boolean isDone;
+}
