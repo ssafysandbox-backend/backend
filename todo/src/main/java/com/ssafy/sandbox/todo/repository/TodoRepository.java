e470d0ff56c381853c8393e4be59979af54783f7
JIYOUNG KIM <kandallee38@gmail.com>
2024-10-17 02:43:33 +0900

    feat: Add query to repository
    Add feature returning query result
    V1: using native sql query
    V2: using PageRequest implemented Pageable

--- todo/src/main/java/com/ssafy/sandbox/todo/repository/TodoRepository.java
+++ todo/src/main/java/com/ssafy/sandbox/todo/repository/TodoRepository.java
@@ -2,8 +2,10 @@ package com.ssafy.sandbox.todo.repository;
 
 import com.ssafy.sandbox.todo.domain.Todo;
 import com.ssafy.sandbox.todo.dto.TodoVO;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -25,4 +27,10 @@ public interface TodoRepository extends JpaRepository<Todo,Long> {
     List<TodoVO> findAllByCursorId(int size, Long cursorId);
 
     boolean existsByIdAfter(Long id);
+
+    @Query(value = "SELECT * FROM todo LIMIT :offset, :limit", nativeQuery = true)
+    List<TodoVO> findAllByOffset(@Param("offset") int offset, @Param("limit") int limit);
+
+    @Query("SELECT t FROM Todo t ORDER BY t.id")
+    List<TodoVO> findAllByOffsetV2(PageRequest pageRequest);
 }
