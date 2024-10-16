1a27229a17e94354ad81bd08042ae4ad6c45df5c
김연수 <rladustn1002@gmail.com>
2024-10-16 15:50:36 +0900

    refactor: cors 설정 변경
    - 허용 도메인 ssafytodo.vercel.app에서 ssafysandbox.vercel.app로 변경

--- src/main/java/com/ssafy/sandbox/config/WebConfig.java
+++ src/main/java/com/ssafy/sandbox/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://ssafytodo.vercel.app")
+                .allowedOrigins("https://ssafysandbox.vercel.app")
                 .allowCredentials(true)
                 .allowedHeaders("*")
                 .allowedMethods("*");
