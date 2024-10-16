24363a3975ea411394571f842ac86f5102fa46e7
JIYOUNG KIM <kandallee38@gmail.com>
2024-10-17 01:51:08 +0900

    feat: Add query statement
    Fix typo about ordering condition
    Add a query finding all todos using cursor id
    Add a query returning boolean if there exists more todos after the cursor id as a result

--- todo/src/main/java/com/ssafy/sandbox/todo/repository/TodoRepository.java
+++ todo/src/main/java/com/ssafy/sandbox/todo/repository/TodoRepository.java
@@ -3,13 +3,14 @@ package com.ssafy.sandbox.todo.repository;
 import com.ssafy.sandbox.todo.domain.Todo;
 import com.ssafy.sandbox.todo.dto.TodoVO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo,Long> {
 
-    List<TodoVO> findAllProjectedByOrderByIdDesc();
+    List<TodoVO> findAllProjectsBy();
 
     @Override
     Todo save(Todo todo);
@@ -19,4 +20,9 @@ public interface TodoRepository extends JpaRepository<Todo,Long> {
 
     @Override
     Optional<Todo> findById(Long todoId);
+
+    @Query("SELECT t FROM Todo t WHERE t.id > ?2 ORDER BY t.id LIMIT ?1")
+    List<TodoVO> findAllByCursorId(int size, Long cursorId);
+
+    boolean existsByIdAfter(Long id);
 }
