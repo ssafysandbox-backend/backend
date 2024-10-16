01ab9fce64e4bd677e28da214355d7a826041e2b
JIYOUNG KIM <kandallee38@gmail.com>
2024-10-17 02:42:54 +0900

    feat: Add feature processing result
    Add feature processing the result to response dto
    V1: using native sql query
    V2: using PageRequest implemented Pageable

--- todo/src/main/java/com/ssafy/sandbox/paging/service/PagingService.java
+++ todo/src/main/java/com/ssafy/sandbox/paging/service/PagingService.java
@@ -1,11 +1,13 @@
 package com.ssafy.sandbox.paging.service;
 
 import com.ssafy.sandbox.paging.dto.PagingCursorIdDto;
+import com.ssafy.sandbox.paging.dto.PagingOffsetDto;
 import com.ssafy.sandbox.todo.dto.TodoVO;
 import com.ssafy.sandbox.todo.repository.TodoRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -33,5 +35,25 @@ public class PagingService {
                 .hasNext(todoRepository.existsByIdAfter(cursorId))
                 .todos(result)
                 .build();
+    }
+
+    public PagingOffsetDto getTodosByOffset(int size, int page) {
+        int offset = size * page;
+        List<TodoVO> result = todoRepository.findAllByOffset(offset, size);
+        boolean hasNext = false;
+        if (!result.isEmpty()) {
+            hasNext = todoRepository.existsByIdAfter(result.get(result.size() - 1).getId());
+        }
+        return new PagingOffsetDto(SUCCESS_MESSAGE, page, result.size(), hasNext, result);
+    }
+
+    public PagingOffsetDto getTodosByOffsetV2(int size, int page) {
+        PageRequest pageRequest = PageRequest.of(page, size);
+        List<TodoVO> result = todoRepository.findAllByOffsetV2(pageRequest);
+        boolean hasNext = false;
+        if (!result.isEmpty()) {
+            hasNext = todoRepository.existsByIdAfter(result.get(result.size() - 1).getId());
+        }
+        return new PagingOffsetDto(SUCCESS_MESSAGE, page, result.size(), hasNext, result);
     }
 }
