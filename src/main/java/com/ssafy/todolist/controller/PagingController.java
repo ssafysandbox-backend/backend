360b4ee266a9ea1748ec3558abaf3f36ce419037
aseongjun99 <aseongjun99@gmail.com>
2024-10-17 21:50:55 +0900

    feat: Add PagingController

---
+++ src/main/java/com/ssafy/todolist/controller/PagingController.java
@@ -0,0 +1,26 @@
+package com.ssafy.todolist.controller;
+
+import com.ssafy.todolist.domain.OffsetResponse;
+import com.ssafy.todolist.service.PagingService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/paging")
+@RestController
+public class PagingController {
+
+    private final PagingService pagingService;
+
+    public PagingController(PagingService pagingService) {
+        this.pagingService = pagingService;
+    }
+
+    @GetMapping("/offset")
+    public ResponseEntity<OffsetResponse> getOffsetPaging(@RequestParam("size") Integer size, @RequestParam("page") Integer page) {
+        OffsetResponse offsetResponse = pagingService.getOffsetPaging(size, page);
+        return ResponseEntity.ok(offsetResponse);
+    }
+}
