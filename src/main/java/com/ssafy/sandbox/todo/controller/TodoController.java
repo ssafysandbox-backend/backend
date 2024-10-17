cb687311fc66e91d06382db6493ed04cb9c85587
leedongkyu0407 <use02012@gmail.com>
2024-10-18 00:33:19 +0900

    refactor: Restore TodoController to original state
    - Reverted TodoController to previous implementation.

--- src/main/java/com/ssafy/sandbox/todo/controller/TodoController.java
+++ src/main/java/com/ssafy/sandbox/todo/controller/TodoController.java
@@ -1,39 +1,37 @@
 package com.ssafy.sandbox.todo.controller;
 
-import com.ssafy.sandbox.todo.domain.Todo;
 import com.ssafy.sandbox.todo.dto.TodoDto;
 import com.ssafy.sandbox.todo.dto.TodoListResponse;
 import com.ssafy.sandbox.todo.service.TodoService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/todos")
 @RequiredArgsConstructor
 public class TodoController {
 
     private final TodoService todoService;
 
-    @GetMapping("/todos")
+    @GetMapping()
     public TodoListResponse listAllTodos(){
         List<TodoDto> todos = todoService.getTodos();
         return new TodoListResponse(todos);
     }
 
-    @PostMapping("/todos")
+    @PostMapping()
     public TodoDto createTodo(@RequestBody TodoDto todoDto){
         return todoService.createTodo(todoDto);
     }
 
-    @DeleteMapping("/todos/{todoId}")
+    @DeleteMapping("/{todoId}")
     public void deleteTodo(@PathVariable Long todoId){
         todoService.deleteTodo(todoId);
     }
 
-    @PatchMapping("/todos/{todoId}")
+    @PatchMapping("/{todoId}")
     public void updateTodo(@PathVariable Long todoId){
         todoService.updateTodo(todoId);
     }
