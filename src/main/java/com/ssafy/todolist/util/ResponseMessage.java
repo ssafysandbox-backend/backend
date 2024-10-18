bd5aef621047b993525c79d8e84e79a1ac0e8ea0
aseongjun99 <aseongjun99@gmail.com>
2024-10-18 19:30:01 +0900

    refactor: Modify enum name

--- src/main/java/com/ssafy/todolist/util/ResponseMessage.java
+++ src/main/java/com/ssafy/todolist/util/ResponseMessage.java
@@ -1,13 +1,13 @@
 package com.ssafy.todolist.util;
 
-public enum Util {
+public enum ResponseMessage {
 
     RESPONSE_SUCCESS("정상적으로 요청되었습니다."),
     RESPONSE_FAIL("정상적이지 않은 요청입니다.");
 
     private final String message;
 
-    private Util(String message) {
+    private ResponseMessage(String message) {
         this.message = message;
     }
 
