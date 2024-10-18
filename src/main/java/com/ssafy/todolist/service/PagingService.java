5c6678dfdd532561b5795973c4b273826402d7ef
aseongjun99 <aseongjun99@gmail.com>
2024-10-18 19:29:17 +0900

    feat: Add cursor paging logic in service

--- src/main/java/com/ssafy/todolist/service/PagingService.java
+++ src/main/java/com/ssafy/todolist/service/PagingService.java
@@ -1,15 +1,18 @@
 package com.ssafy.todolist.service;
 
+import com.ssafy.todolist.domain.CursorResponse;
 import com.ssafy.todolist.domain.OffsetResponse;
 import com.ssafy.todolist.domain.Todo;
 import com.ssafy.todolist.domain.TodoDTO;
 import com.ssafy.todolist.repository.TodoRepository;
-import com.ssafy.todolist.util.Util;
+import com.ssafy.todolist.util.ResponseMessage;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class PagingService {
@@ -24,7 +27,16 @@ public class PagingService {
     public OffsetResponse getOffsetPaging(int size, int page) {
         Pageable pageable = PageRequest.of(page-1,size);
         Page<Todo> p = todoRepository.findAll(pageable);
-        return new OffsetResponse(Util.RESPONSE_SUCCESS.getMessage(), page, size, p.getTotalPages(), p.hasNext(), p.hasPrevious(),  p.stream().map(TodoDTO::of).toList());
+        return OffsetResponse.build(ResponseMessage.RESPONSE_SUCCESS.getMessage(), page, size, p);
+//        return new OffsetResponse(ResponseMessage.RESPONSE_SUCCESS.getMessage(), page, size, p.getTotalPages(), p.hasNext(), p.hasPrevious(),  p.stream().map(TodoDTO::of).toList());
+    }
+
+    @Transactional(readOnly = true)
+    public CursorResponse getCursorPaging(int size, int cursorId) {
+        List<TodoDTO> todos = todoRepository.findTodosByCursor(size, cursorId).stream().map(TodoDTO::of).toList();
+        int lastId = todos.get(todos.size()-1).id();
+        boolean hasNext = todoRepository.existsByIdAfter(lastId);
+        return new CursorResponse(ResponseMessage.RESPONSE_SUCCESS.getMessage(), lastId, size, hasNext, todos);
     }
 
 }
