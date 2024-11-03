94e6451f2d6af6d5ca177995008b9acf0c7c6215
JIYOUNG KIM <kandallee38@gmail.com>
2024-11-04 02:50:56 +0900

    feat: Add issue and reissue
    Add issue access and refresh token method
    Add reissue token method using refresh token

--- todo/src/main/java/com/ssafy/sandbox/user/controller/UserController.java
+++ todo/src/main/java/com/ssafy/sandbox/user/controller/UserController.java
@@ -1,16 +1,18 @@
 package com.ssafy.sandbox.user.controller;
 
-import com.ssafy.sandbox.user.dto.KakaoUserInfoResponseDto;
-import com.ssafy.sandbox.user.dto.LoginRequestDto;
+import com.ssafy.sandbox.user.dto.*;
 import com.ssafy.sandbox.user.service.OauthService;
 import com.ssafy.sandbox.user.service.UserService;
+import com.ssafy.sandbox.user.util.JWTErrorCode;
+import io.jsonwebtoken.JwtException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @Controller
@@ -31,8 +33,46 @@ public class UserController {
         String accessToken = oauthService.getAccessToken(dto.getCode());
         log.debug("access token: {}", accessToken);
         KakaoUserInfoResponseDto userInfo = oauthService.getUserInfo(accessToken);
-//        userService
+        LoginResponseDto result = userService.getToken(userInfo);
+        return ResponseEntity.ok().body(result);
+    }
 
-        return ResponseEntity.ok().build();
+    @GetMapping("/member")
+    public ResponseEntity<?> getNickname(
+            @RequestHeader(value = HttpHeaders.AUTHORIZATION, required = false) String token) {
+        if (token == null) {
+            return ResponseEntity.badRequest().build();
+        }
+        log.debug("token: {}", token);
+        try {
+            VerifyTokenResponseDto response = userService.getNickname(token);
+            return ResponseEntity.ok().body(response);
+        } catch (JwtException e) {
+            if (e.getMessage().equals(JWTErrorCode.INVALID_TOKEN.getMessage())) {
+                return ResponseEntity.notFound().build();
+            }
+
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+    }
+
+    @GetMapping("/reissue")
+    public ResponseEntity<?> reissue(
+            @RequestHeader(value = "X-Refresh", required = false) String token) {
+        if (token == null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        log.debug("token: {}", token);
+        try {
+            ReIssueTokenDto response = userService.reissueToken(token);
+            return ResponseEntity.ok().body(response);
+        } catch (JwtException e) {
+            if (e.getMessage().equals(JWTErrorCode.INVALID_TOKEN.getMessage())) {
+                return ResponseEntity.notFound().build();
+            }
+
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
     }
 }
