c195ab372db032e86ddccc13803d53e57ceb8192
JIYOUNG KIM <kandallee38@gmail.com>
2024-11-04 02:51:41 +0900

    feat: Add response dto
    Add response dto having access token and refresh token

---
+++ todo/src/main/java/com/ssafy/sandbox/user/dto/VerifyTokenResponseDto.java
@@ -0,0 +1,11 @@
+package com.ssafy.sandbox.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class VerifyTokenResponseDto {
+
+    private String nickname;
+}
